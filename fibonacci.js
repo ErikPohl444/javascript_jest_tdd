b72f78ba8a8991fb2b1b657eefc253bd2bdb85bf
Erik Pohl <epohl@dmigs.com>
2025-01-08 17:14:45 -0500

    applied prettier for linting

--- fibonacci.js
+++ fibonacci.js
@@ -1,11 +1,8 @@
-function fibonacci(index ) {
-    if (index > 1)
-        return fibonacci(index-1) + fibonacci(index-2)
-    if (index == 0)
-        return 0;
-    if (index == 1)
-        return 1;
-  }
+function fibonacci(index) {
+  if (index > 1) return fibonacci(index - 1) + fibonacci(index - 2);
+  if (index == 0) return 0;
+  if (index == 1) return 1;
+}
 module.exports = fibonacci;
-  
-console.log(fibonacci(0))
+
+console.log(fibonacci(0));
