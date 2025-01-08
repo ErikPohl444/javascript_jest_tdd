546e4c4537146100562a12e25ae71ee497e14400
Erik Pohl <epohl@dmigs.com>
2025-01-08 16:37:28 -0500

    initial commit

---
+++ fibonacci.js
@@ -0,0 +1,11 @@
+function fibonacci(index ) {
+    if (index > 1)
+        return fibonacci(index-1) + fibonacci(index-2)
+    if (index == 0)
+        return 0;
+    if (index == 1)
+        return 1;
+  }
+module.exports = fibonacci;
+  
+console.log(fibonacci(0))
---
+++ fibonacci.test.js
@@ -0,0 +1,21 @@
+const fibonacci = require('./fibonacci');
+
+test('fibonacci index 0 = 0', () => {
+  expect(fibonacci(0)).toBe(0);
+});
+
+test('fibonacci index 1 = 1', () => {
+    expect(fibonacci(1)).toBe(1);
+  });
+
+test('fibonacci index 2 = 1', () => {
+    expect(fibonacci(2)).toBe(1);
+  });
+
+test('fibonacci index 3 = 2', () => {
+    expect(fibonacci(3)).toBe(2);
+  });
+
+test('fibonacci index 4 = 3', () => {
+    expect(fibonacci(4)).toBe(3);
+  });
